ca6920b3be80657325be4e6c80e2258a03f6c07c
snadi <nadi@ualberta.ca>
2018-01-09 14:46:43 -0700

    Add addition functionality

--- test.c
+++ test.c
@@ -2,9 +2,11 @@
 
 int main(){
 
+    int x= 7, y = 10;
+    
     printf("Hello there again\n");
 
-    printf("Adding a problematic print statement\n");
-
+    printf("Sum of x and y:%d\n", x + y);
+    
     return 0;
 }
